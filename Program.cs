8562727953ec5c571f410cb04213269a842abe19
Pedro-Jorge-de-Abreu <pedrojorge455@outlook.com>
2026-04-28 16:03:23 -0300

    Add files via upload
    Classe ContaBancaria

--- Program.cs
+++ Program.cs
@@ -1,15 +1,22 @@
-﻿namespace SistemaEscolar
+﻿namespace banco
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Aluno aluno = new Aluno();
-            aluno.ReceberDados();
-            aluno.CalcularMedia();
-            aluno.ImprimirNotaFinal();
-            aluno.CalcularNotaFinal();
-            Console.WriteLine("\nPressione qualquer tecla para sair...");
+
+            ContaBancaria minhaConta = new ContaBancaria();
+
+            minhaConta.ReceberDados();
+            minhaConta.MostrarSaldo();
+
+            minhaConta.Depositar();
+            minhaConta.MostrarSaldo();
+
+            minhaConta.Sacar();
+            minhaConta.MostrarSaldo();
+
+            Console.WriteLine("\nPressione qualquer tecla para encerrar...");
             Console.ReadKey();
         }
     }
