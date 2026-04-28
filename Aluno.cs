c0c039bc94f0f553cd799e1f176eb616f68ee53f
Pedro-Jorge-de-Abreu <pedrojorge455@outlook.com>
2026-04-28 15:55:15 -0300

    Add files via upload
    Classe Aluno e Notas

---
+++ Aluno.cs
@@ -0,0 +1,51 @@
+﻿namespace SistemaEscolar
+{
+    public class Aluno
+    {
+        public string RA { get; set; }
+        public string Nome { get; set; }
+        public double NotaProva { get; set; }
+        public double NotaTrabalho { get; set; }
+        public double NotaFinal { get; set; }
+
+        public void ReceberDados()
+        {
+            Console.Write("Digite o RA: ");
+            RA = Console.ReadLine();
+
+            Console.Write("Digite o Nome: ");
+            Nome = Console.ReadLine();
+
+            Console.Write("Digite a Nota da Prova: ");
+            NotaProva = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Nota do Trabalho: ");
+            NotaTrabalho = double.Parse(Console.ReadLine());
+        }
+
+        public void CalcularMedia()
+        {
+            NotaFinal = (NotaProva + NotaTrabalho) / 2;
+        }
+
+        public bool CalcularNotaFinal()
+        {
+            if (NotaFinal >= 6.0)
+            {
+                Console.WriteLine("Aluno aprovado diretamente!");
+                return true;
+            }
+            else
+            {
+                double necessaria = 10.0 - NotaFinal;
+                Console.WriteLine($"O aluno precisa de {necessaria:F2} na prova final.");
+                return false;
+            }
+        }
+
+        public void ImprimirNotaFinal()
+        {
+            Console.WriteLine($"\nNota Final (Média): {NotaFinal:F2}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SistemaEscolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            aluno.ReceberDados();
+            aluno.CalcularMedia();
+            aluno.ImprimirNotaFinal();
+            aluno.CalcularNotaFinal();
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
