777db95f9160ce5f03b329bbf89090d66d5d997a
Pedro-Jorge-de-Abreu <pedrojorge455@outlook.com>
2026-04-28 16:13:18 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
-﻿namespace SistemaEscolar
+﻿namespace CalculoSalario
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Aluno aluno = new Aluno();
-            aluno.ReceberDados();
-            aluno.CalcularMedia();
-            aluno.ImprimirNotaFinal();
-            aluno.CalcularNotaFinal();
+            CalculadoraDeSalario func = new CalculadoraDeSalario();
+
+            func.ReceberDados();
+            func.MostrarSalario();
+            Console.Write("Digite o percentual de AUMENTO (ex: 10): ");
+            double percAumento = double.Parse(Console.ReadLine());
+            func.CalcularAumento(percAumento);
+            func.MostrarSalario();
+            Console.Write("Digite o percentual de DESCONTO (ex: 5): ");
+            double percDesconto = double.Parse(Console.ReadLine());
+            func.CalcularDesconto(percDesconto); 
+
+            func.MostrarSalario();
+
             Console.WriteLine("\nPressione qualquer tecla para sair...");
             Console.ReadKey();
         }
